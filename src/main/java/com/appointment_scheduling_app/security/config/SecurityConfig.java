789626ffbdc962581fd6dc2d68da81e4fad8d7e8
Ananda Magar <anandakmagar@gmail.com>
2024-06-16 00:22:03 -0600

    first commit

--- src/main/java/com/appointment_scheduling_app/security/config/SecurityConfig.java
+++ src/main/java/com/appointment_scheduling_app/security/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 //                        .anyRequest().authenticated()
 //                )
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/security-ms-59cfc31741b3.herokuapp.com/auth/**").permitAll()
+                        .requestMatchers("http://security/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
